d1f1b20d162417d80dc2c26ab9a97cd5cc7f07b1
Rafał Lalik <rafal.lalik@uj.edu.pl>
2020-02-25 15:26:59 +0100

    Add extra cmake scripts

--- python/setup.py
+++ python/setup.py
@@ -5,68 +5,68 @@ modules = [
     Extension(
         "mgeanttrack",
         sources=[
-            "/home/rafal/projects/MAPT-clean/python/mgeanttrack.pyx",
+            "/home/dziadu/projects/sifi-framework/python/mgeanttrack.pyx",
         ],
         language="c++",              # generate C++ code
         extra_compile_args=["-std=c++14"],
-        include_dirs=["/usr/lib64/root/6.14/include"],
-        library_dirs=["/home/rafal/projects/MAPT-clean/build/lib"],
+        include_dirs=["/usr/lib/root/6.18/include"],
+        library_dirs=["/home/dziadu/projects/sifi-framework/build/lib"],
         libraries=["GeantSim", "MAPT"],
     ),
     Extension(
         "mgeantfibersraw",
         sources=[
-            "/home/rafal/projects/MAPT-clean/python/mgeantfibersraw.pyx",
+            "/home/dziadu/projects/sifi-framework/python/mgeantfibersraw.pyx",
         ],
         language="c++",              # generate C++ code
         extra_compile_args=["-std=c++14"],
-        include_dirs=["/usr/lib64/root/6.14/include"],
-        library_dirs=["/home/rafal/projects/MAPT-clean/build/lib"],
+        include_dirs=["/usr/lib/root/6.18/include"],
+        library_dirs=["/home/dziadu/projects/sifi-framework/build/lib"],
         libraries=["GeantSim", "MAPT"],
     ),
     Extension(
         "mfibersstackcalsim",
         sources=[
-            "/home/rafal/projects/MAPT-clean/python/mfibersstackcalsim.pyx",
+            "/home/dziadu/projects/sifi-framework/python/mfibersstackcalsim.pyx",
         ],
         language="c++",              # generate C++ code
         extra_compile_args=["-std=c++14"],
-        include_dirs=["/usr/lib64/root/6.14/include"],
-        library_dirs=["/home/rafal/projects/MAPT-clean/build/lib"],
+        include_dirs=["/usr/lib/root/6.18/include"],
+        library_dirs=["/home/dziadu/projects/sifi-framework/build/lib"],
         libraries=["FibersStack", "MAPT"],
     ),
     Extension(
         "mfibersstackcal",
         sources=[
-            "/home/rafal/projects/MAPT-clean/python/mfibersstackcal.pyx",
+            "/home/dziadu/projects/sifi-framework/python/mfibersstackcal.pyx",
         ],
         language="c++",              # generate C++ code
         extra_compile_args=["-std=c++14"],
-        include_dirs=["/usr/lib64/root/6.14/include"],
-        library_dirs=["/home/rafal/projects/MAPT-clean/build/lib"],
+        include_dirs=["/usr/lib/root/6.18/include"],
+        library_dirs=["/home/dziadu/projects/sifi-framework/build/lib"],
         libraries=["FibersStack", "MAPT"],
     ),
     Extension(
         "mapt_python",
         sources=[
-            "/home/rafal/projects/MAPT-clean/python/mapt_python.pyx",
-            "/home/rafal/projects/MAPT-clean/python/MAPT.cpp",
+            "/home/dziadu/projects/sifi-framework/python/mapt_python.pyx",
+            "/home/dziadu/projects/sifi-framework/python/MAPT.cpp",
         ],
         language="c++",              # generate C++ code
         extra_compile_args=["-std=c++14"],
         include_dirs=[
-            "/home/rafal/projects/MAPT-clean/lib/base",
-            "/home/rafal/projects/MAPT-clean/lib/geant",
-            "/home/rafal/projects/MAPT-clean/lib/fibers_stack",
-            "/usr/lib64/root/6.14/include"
+            "/home/dziadu/projects/sifi-framework/lib/base",
+            "/home/dziadu/projects/sifi-framework/lib/geant",
+            "/home/dziadu/projects/sifi-framework/lib/fibers_stack",
+            "/usr/lib/root/6.18/include"
         ],
-        library_dirs=["/home/rafal/projects/MAPT-clean/build/lib"],
+        library_dirs=["/home/dziadu/projects/sifi-framework/build/lib"],
         libraries=["FibersStack", "GeantSim", "MAPT"],
     ),
 ]
 
 setup(
     name='pyMAPT',
-    package_dir={'' : '/home/rafal/projects/MAPT-clean/python' },
-    ext_modules=cythonize(modules, build_dir="/home/rafal/projects/MAPT-clean/build/python")
+    package_dir={'' : '/home/dziadu/projects/sifi-framework/python' },
+    ext_modules=cythonize(modules, build_dir="/home/dziadu/projects/sifi-framework/build/python")
 )
