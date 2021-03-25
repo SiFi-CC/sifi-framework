1f24dec05235f193cb36cfa01d46f134619d1ac5
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2021-03-25 16:44:08 +0100

    seprate scripts to generate params.txt for PMI/DD, Osc

---
+++ macros/merge_params_DD_Osc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat params_DD_Osc/FibersGeomPar.txt
+cat params_DD_Osc/FibersDDUnpackerPar.txt
+cat params_DD_Osc/FibersDDLookupTable.txt
+cat params_DD_Osc/FibersPMILookupTable.txt
+cat params_DD_Osc/FibersDDCalibratorPar.txt
+cat params_DD_Osc/FibersCalibratorPar.txt
+cat params_DD_Osc/FibersHitFinderPar.txt
+cat params_DD_Osc/FibersClusterFinderPar.txt
+cat params_DD_Osc/FibersDigitizerPar.txt
+cat params_DD_Osc/FibersHitFinderFiberPar.txt
---
+++ macros/merge_params_PMI.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat params_PMI/FibersGeomPar.txt
+cat params_PMI/FibersDDUnpackerPar.txt
+cat params_PMI/FibersDDLookupTable.txt
+cat params_PMI/FibersPMILookupTable.txt
+cat params_PMI/FibersDDCalibratorPar.txt
+cat params_PMI/FibersCalibratorPar.txt
+cat params_PMI/FibersHitFinderPar.txt
+cat params_PMI/FibersClusterFinderPar.txt
+cat params_PMI/FibersDigitizerPar.txt
+cat params_PMI/FibersHitFinderFiberPar.txt
