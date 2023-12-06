ad3fd1b474e188435c1d59675103213a7c3f6641
kasia-rusiecka <katarzyna.rusiecka@doctoral.uj.edu.pl>
2023-12-06 14:32:13 +0100

    framework prepared for implementation of 4-to-1 simulation importer

---
+++ lib/base/datasources/SSimSource.h
@@ -0,0 +1,12 @@
+#ifndef SSIMSOURCE_H
+#define SSIMSOURCE_H
+
+#include "sifi_export.h" 
+#include "SRootSource.h"
+
+class SIFI_EXPORT SSimSource : public SRootSource
+{
+
+};
+
+#endif /* SSIMSOURCE_H */
